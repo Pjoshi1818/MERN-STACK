d62d92359e4de9ef4a5b68fe786ceaaa8e79b749
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-09-10 10:50:56 +0530

    DOM

---
+++ JS/DOM/scprit.js
@@ -0,0 +1,44 @@
+// // DOM 
+
+
+
+// let l = document.getElementById('box')
+// console.log(l);
+// l.id = "box1";
+// console.log(`   class name is  =  ${l.className}`);
+// l.className = "class1";
+// console.log(l.className)
+
+// console.log(l.style)
+// l.style.backgroundColor = " red"
+// l.style.borderRadius = "50%"
+// l.classList.add = "class2"
+// console.log(l.classList);
+
+// l.classList.toggle("class1")
+
+
+// let mylink = document.getElementById("link")
+// console.log(mylink);
+
+
+// mylink.href =  "https://jionet2.jio.in:8443/";
+
+// let input = document.getElementById("password")
+// input.type = "text";
+
+
+ let mydiv = document.getElementById('box')
+
+ mydiv.setAttribute("style","background:yellow")
+ mydiv.setAttribute("style","background:brown")   // replace
+
+
+// innerHtml , innerText , TextContent
+// mydiv.innerHTML = " pankaj joshi "   
+// mydiv.innerHTML = " <input id='pass' type='password'>"
+let newpara = document.getElementById('para')
+console.log(newpara.textContent);
+
+// mydiv.innerText = " lalit "
+mydiv,textContent = "jayesh "
