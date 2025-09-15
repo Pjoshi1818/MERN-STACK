8f761ca579000bdcef8c4560124d5a08ae359a67
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-09-15 22:11:31 +0530

    2. ES Modules (ESM)

--- JS/day22/function.js
+++ JS/day22/function.js
@@ -1,4 +1,12 @@
-function sum(a, b) {
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
+
+
+
+
+
+export default function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
--- JS/day22/index.js
+++ JS/day22/index.js
@@ -3,9 +3,16 @@
             // **************************** 1. CommonJS (CJS) ***************************
 
                 //   **   Used in Node.js (older system) **
-const sum = require("./function.js");
+
+
+                // const sum = require("./function.js");
+                // console.log(sum(2, 3));
+
+
+
+
+//second     
+              // ************************** 2. ES Modules (ESM) ************************************
+              // Standardized in modern JavaScript & browsers
+ import { sum } from "./function.js";
 console.log(sum(2, 3));
-
-
-
-
