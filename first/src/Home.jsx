944d48f896ca0146d3e6c3ce7e0baf98430a8e88
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-09-16 22:42:28 +0530

    notes ALL

---
+++ first/src/Home.jsx
@@ -0,0 +1,13 @@
+//   function Home(){
+//     return(
+//       <>  
+//        <div>Home </div>
+//         <p>
+//             Lorem ipsum dolor sit amet.
+//         </p>
+//       </> 
+//     )
+//   }
+
+
+  export default Home;
